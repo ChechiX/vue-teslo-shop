37afc73708a0d3e8a85e65d3673cec72c7b6ac49
Julio Anccasi <julio.anccasi.dev@gmail.com>
2024-11-11 09:11:45 -0500

    First commit

---
+++ src/modules/auth/guards/is-admin.guard.ts
@@ -0,0 +1,17 @@
+import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
+import { useAuthStore } from '../stores/auth.store';
+
+const isAdminGuard = async (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext,
+) => {
+  const authStore = useAuthStore();
+
+  // console.log(to);
+  await authStore.checkAuthStatus();
+
+  authStore.isAdmin ? next() : next({ name: 'home' });
+};
+
+export default isAdminGuard;
---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- component -->
+  <!-- Create By Joker Banny -->
+  <!-- Header Navbar -->
+  <top-menu />
+
+  <!-- RouterView -->
+  <router-view />
+
+  <!-- Footer -->
+  <custom-footer />
+</template>
+
+<script lang="ts" setup>
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
